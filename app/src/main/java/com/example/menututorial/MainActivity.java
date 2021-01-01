73b112d202190e6a883415a40644ed36e632ec81
mamba007 <ga1998urav@gmail.com>
2021-01-01 10:45:49 +0530

    First Commit

---
+++ app/src/main/java/com/example/menututorial/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.menututorial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_activity,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_profile:
+                Toast.makeText(this,"Clicked on user profile",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_share:
+                Toast.makeText(this,"Clicked on share",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
